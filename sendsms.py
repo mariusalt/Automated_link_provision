0928569b144aa5c1a0c5d47c02d9edc889a3fbb7
mariusaltalt <30729713+mariusaltalt@users.noreply.github.com>
2021-03-02 18:45:21 +0100

    anonymized

--- sendsms.py
+++ sendsms.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-client = Client("AC3a59d1287e0b5eda97371ba0ac23ec2f", "a7437027a342f3118d59311607db228b")
+client = Client("XX", "XX")
 
 
 num=[
-"+4915731109072",
-"+491717818776",
-"+4917655285817",
-"+491756224910",
-"+4915226882005"
+"+123456789",
+"+123456789",
+"+123456789",
+"+123456789",
+"+123456789"
 ]
 
 
@@ -16,5 +16,5 @@ num=[
 
 for i in num:
     client.messages.create(to=i, 
-                        from_="+17044626748", 
+                        from_="+123456789", 
                         body="Reminder: MLab-Onlinestudie, morgen um 14:30 Uhr. Der Zoom-Link wurde per E-Mail zugesandt. Wir freuen uns auf Sie!")
